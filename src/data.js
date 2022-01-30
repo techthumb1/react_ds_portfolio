053930366d897a94e2edd959bf4395c4e596f8fc
Jason Robinson <robinsonjason761@gmail.com>
2022-01-30 17:25:14 -0600

    adding component files

--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Scribble Stadium",
+    subtitle: "Tesseract Hyperparameter Tuning",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A machine learning project that uses a genetic algorithm to optimize the hyperparameters of a custom Tesseract OCR model.",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/BloomTech-Labs/scribble-stadium-ds",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Airbnb Price Detector",
+    subtitle: "Airbnb - NLP",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "Using LDAModel to detect the price of a room in a given location.",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/techthumb1/airbnb-price-detector,
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Twitoff",
+    subtitle: "Twitoff-Dspt6",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A Twitter machine learning model that predicts the probability of the tweet being from a specific user.",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/techthumb1/Twitoff-dspt6",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "RPT Generator",
+    subtitle: "Auto-Generated RPTs",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A tool that generates research paper titles using a custom arxivscraper.",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/techthumb1/rpt_generator",
   },
 ];
 
@@ -38,23 +38,26 @@ export const testimonials = [
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
     image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
+    name: "Reginald Hicks",
+    company: "Prixym",
   },
   {
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
     image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
+    name: "Paul Nowazyck",
+    company: "shOUTer",
   },
 ];
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
+  "Python",
+  "Machine Learning",
+  "TensorFlow",
+  "Flask",
+  "SQL",
+  "Scikit-Learn",
+  "Docker",
+  "Deep Learning",
+  "SAP BI"
 ];
