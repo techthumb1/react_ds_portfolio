8c0f89a3d17c3cf2df607b0f7af2b3c435dbd24c
Jason Robinson <robinsonjason761@gmail.com>
2022-02-26 12:40:17 -0600

    change to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-import React from "react";
 
-export default function App() {
-  return "App";
+// We need to import the CSS so that webpack will load it.
+import React from 'react'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>My Portfolio</h1>
+        <p>Portfolio of data science, data engineering, 
+          and software development projects</p>
+      </div>
+    )
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import React from "react";
+//
+//export default function App() {
+//  return "App";
+//}
