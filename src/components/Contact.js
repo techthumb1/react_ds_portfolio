c3e5070e1390573da25399b64f77ff05a70e536d
Jason Robinson <49352529+techthumb1@users.noreply.github.com>
2022-03-12 21:24:30 -0600

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,7 +31,7 @@ export default function Contact() {
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
-            width="50%"
+            width="100%"
             height="50%"
             title="map"
             className="absolute inset-0"
