dc7d7b4f854b8876be8377d0803ad45f318e72f5
Jason Robinson <robinsonjason761@gmail.com>
2022-03-18 23:49:27 -0500

    Adding blog and article links

---
+++ src/blogs.js
@@ -0,0 +1,9 @@
+export const projects = [
+    {
+      title: "What Are The Top 5 States That Are Spending The Most Money",
+      subtitle: "Personal Consumer Expenditures",
+      description:
+        "A brief examination of top areas within the U.S. that are spending the most money on consumer goods.",
+      image: "./tesseract_cube.gif",
+      link: "https://medium.com/@robinsonjason761/what-are-the-top-5-states-that-are-spending-the-most-money-ad41d0ba6843",
+    },
