1ab35ca1c9eb4f5729ee446fb885b0c96d289fdb
Jason Robinson <robinsonjason761@gmail.com>
2022-04-10 10:57:27 -0500

    Update to blog

--- src/blogs.js
+++ src/blogs.js
@@ -15,4 +15,18 @@ export const blogs = [
       image: "./topic.png",
       link: "https://medium.com/@robinsonjason761/nlp-for-topic-modeling-with-a-lda-technique-1fe4bec4e4096-+920",
     },
+          title: "An Analysis of Linear Regression",
+      subtitle: "COMING SOON",
+      description:
+        "A brief analysis of predicting phone purchase costs over a 20-year period for each adult consumer.",
+      image: "./regression.png",
+      link: "https://medium.com/@robinsonjason761"
+    },
+    {
+      title: "Box-Jenkins ARIMA Method for Time Series Forecasting",
+      subtitle: "COMING SOON",
+      description:
+        "The Box-Jenkins ARIMA method is examined when applying on CPI time series covering the perod 2011 - 2021.",
+      image: "./arima.png",
+      link: "https://medium.com/@robinsonjason761",
 ]
--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const projects = [
     title: "Airbnb Price Detector",
     subtitle: "Airbnb - NLP",
     description:
-      "Using LDAModel to detect the price of a room in a given location.",
+      "Incorporating a Latent Dirichlet Allocation model to detect the price of a room in a given location in NYC.",
     image: "./airbnb_2.gif",
     link: "https://github.com/techthumb1/airbnb-price-detector",
   },
@@ -19,7 +19,7 @@ export const projects = [
     title: "Twitoff",
     subtitle: "Twitoff-Dspt6",
     description:
-      "A Twitter machine learning model using predictive analysis in using the Twitter Api to predict the probability of a tweet being posted by specific user.",
+      "A Twitter machine learning model using predictive analysis in using the Twitter Api to predict the probability related to a user's tweet.",
     image: "./twitoff.gif",
     link: "https://github.com/techthumb1/Twitoff-dspt6",
   },
@@ -27,7 +27,7 @@ export const projects = [
     title: "Kickstarter Success", 
     subtitle: "Kickstarter Campaign",
     description:
-      "Kickstarter campaign project that consists of analyzing the most prominent features that exist within successful campaigns to determine the positivity rate of a specific campaign genrating funds.",
+      "Kickstarter campaign project that consists of analyzing the most prominent features that exist within successful campaigns.",
     image: "./kickstarter.gif",
     link: "https://github.com/techthumb1/kickstarter_success",
   },
