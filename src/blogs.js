0b4e2d00dc5550f6a1556e4c89438e7216d73b1b
Jason Robinson <robinsonjason761@gmail.com>
2022-04-12 22:31:29 -0500

    Updating blog

--- src/blogs.js
+++ src/blogs.js
@@ -16,8 +16,8 @@ export const blogs = [
       link: "https://medium.com/@robinsonjason761/nlp-for-topic-modeling-with-a-lda-technique-1fe4bec4e4096-+920"
     },
     {
-      title: "An Analysis of Linear Regression for predictive analysis",
-      subtitle: "An overview of linear regression",
+      title: "An Analysis of Linear Regression for Predictive Analysis",
+      subtitle: "An Overview of Linear Regression",
       description:
         "A brief analysis of predicting phone purchase costs over a 20-year period for each adult consumer.",
       image: "./coming_soon.png",
