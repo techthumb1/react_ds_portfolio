002b6a54fdc058808f9e6511b981f063ead85a75
Jason Robinson <robinsonjason761@gmail.com>
2022-04-12 22:46:46 -0500

    Updating blog

--- src/data.js
+++ src/data.js
@@ -17,7 +17,7 @@ export const projects = [
   },
   {
     title: "Twitoff",
-    subtitle: "Twitoff-Dspt6",
+    subtitle: "Twitoff-Ds",
     description:
       "A Twitter machine learning model using predictive analysis in using the Twitter Api to predict the probability related to a user's tweet.",
     image: "./twitoff.gif",
