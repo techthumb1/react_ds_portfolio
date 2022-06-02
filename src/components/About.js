84587be2a587866fd33a136c2d3bfe58059c9abb
Jason Robinson <robinsonjason761@gmail.com>
2022-06-01 19:11:20 -0500

    Making minimal changes to data.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Jason.
-            <br className="hidden lg:inline-block" />I enjoy building structured and insightful models.
+            <br className="hidden lg:inline-block" /> I enjoy building structured and insightful models.
           </h1>
           <p className="mb-8 leading-relaxed">
            I thrive in my ability to take complex problems and turn them into efficient,
